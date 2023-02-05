c68254769f62428bb6b208fb945a55c11f88ccf4
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-02-05 14:42:17 -0300

    feat: network

--- js/Car/car.js
+++ js/Car/car.js
@@ -11,9 +11,11 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
     this.demaged = false;
+    this.useBrain = controlType === "AI";
 
     if (controlType !== "DUMMY") {
       this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
 
     this.controls = new Controls(controlType);
@@ -28,6 +30,18 @@ class Car {
 
     if (this.sensor) {
       this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map((s) =>
+        s === null ? 0 : 1 - s.offset
+      );
+
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+      }
     }
   }
 
